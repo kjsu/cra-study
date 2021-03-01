867b75c2f798d7f27d7b82335479c559a55550ed
kjsu <surtne@gmail.com>
2021-03-01 17:12:20 +0900

    function -> class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,8 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
