19470c6f762975a6440658938815ae2f955a205f
kjsu <surtne@gmail.com>
2021-03-04 23:19:50 +0900

    state

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 import Toc from './components/Toc'
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      toc:{title:"Hello", desc:"World"}
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Toc title="hello" desc="world"></Toc>
+        <Toc title={this.state.toc.title} desc={this.state.toc.desc}></Toc>
       </div>
     );
   }
