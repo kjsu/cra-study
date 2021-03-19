29c7db31716e136b29f81fb490a99f6f20dfaf55
kjsu <surtne@gmail.com>
2021-03-19 23:12:31 +0900

    convention 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css';
 import Subject from './components/Subject'
 import Toc from './components/Toc'
 
-class App extends React.Component {
+class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      mode:'welcome',
       subject:{title:"Web", sub:"World Wide Web!"},
+      welcome:{title:'welcome', desc:'hello react'},
       contents:[
         {id:1, title:"Html", desc:"Html is ..."},
         {id:2, title:"CSS", desc:"CSS is ..."},
