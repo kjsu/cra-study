5b4549256450b02df655ff48857763c862d8550f
kjsu <surtne@gmail.com>
2021-03-27 04:27:30 +0900

    updateComponent 및 update 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Control from './components/Control'
 import Toc from './components/Toc'
 import ReadContent from './components/ReadContent'
 import CreateContent from './components/CreateContent'
+import UpdateContent from './components/UpdateContent'
 
 class App extends Component {
   constructor(props){
@@ -22,39 +23,63 @@ class App extends Component {
     }
   }
 
-  render() {
-    var _title, _desc, _article = null;
+  getReadContent() {
+    var i = 0;
+    while(i < this.state.contents.length){
+      var data = this.state.contents[i];
+      if (data.id === this.state.selected_content_id) {
+        return data;
+      }
+      i = i + 1;
+    }
+  }
 
+  getContent() {
+    var _title, _desc, _article = null;
     if (this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if (this.state.mode === 'read'){
-      var i = 0;
-      while(i < this.state.contents.length){
-        var data = this.state.contents[i];
-        if (data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i + 1;
-      }
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
     } else if (this.state.mode === 'create'){
       _article = <CreateContent onSubmit={function(_title, _desc){
         this.max_content_id = this.max_content_id + 1;
-        var _contents = this.state.contents.concat({
+        var _contents = Array.from(this.state.contents);
+        _contents.push({
           id:this.max_content_id,
           title:_title,
           desc:_desc,
         });
         this.setState({
-          contents:_contents
+          contents:_contents,
+          mode:'read',
+          selected_content_id:this.max_content_id
         });
       }.bind(this)}></CreateContent>
+    } else if (this.state.mode === 'update'){
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={function(_id, _title, _desc){
+        var _contents = Array.from(this.state.contents);
+        var i = 0;
+        while(i < _contents.length){
+          if(_contents[i].id === _id){
+            _contents[i] = {id:_id, title:_title, desc:_desc}
+            break;
+          }
+          i = i + 1;
+        }
+        this.setState({
+          contents:_contents,
+          mode:'read'
+        });
+      }.bind(this)}></UpdateContent>
     }
+    return _article;
+  }
 
+  render() {
     return (
       <div className="App">
         <Subject
@@ -78,7 +103,7 @@ class App extends Component {
             mode:mode
           })
         }.bind(this)}></Control>
-        {_article}
+        {this.getContent()}
       </div>
     );
   }
